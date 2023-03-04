e3783b59c6ca7439b3b15719ecf22eed7ee75eb7
Hunter Johnston <johnstonhuntera@gmail.com>
2023-03-03 21:15:03 -0500

    update chat message

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -7,11 +7,14 @@
 <div class="chat {type === 'user' ? 'chat-end' : 'chat-start'} justify-end">
 	<div class="chat-image avatar">
 		<div class="w-10 rounded-full">
-			<img src="https://i.pravatar.cc/40?img={type === 'user' ? 1 : 22}" alt="Bot avatar" />
+			<img
+				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Me' : 'B'}"
+				alt="{type} avatar"
+			/>
 		</div>
 	</div>
 	<div class="chat-header">
-		{type === 'user' ? 'You' : 'Bot'}
+		{type === 'user' ? 'Me' : 'Bot'}
 	</div>
 	<div class="chat-bubble {type === 'user' ? 'chat-bubble-primary' : 'chat-bubble-secondary'}">
 		{message}
