466fc708809c95f4d7ad5934487cea1fb74ba601
Hunter Johnston <johnstonhuntera@gmail.com>
2023-03-03 22:12:51 -0500

    clear stuff

--- src/lib/chat.ts
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const chatHistory = writable<{ query: string; answer: string }[]>([])
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,91 +1,23 @@
 <script lang="ts">
 	import ChatMessage from '$lib/components/ChatMessage.svelte'
-	import type { ChatCompletionRequestMessage } from 'openai'
-	import { SSE } from 'sse.js'
-
-	let query: string = ''
-	let answer: string = ''
-	let loading = false
-	let chatMessages: ChatCompletionRequestMessage[] = []
-	let scrollToDiv: HTMLDivElement
-
-	function scrollToBottom() {
-		setTimeout(function () {
-			scrollToDiv.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
-		}, 100)
-	}
-
-	const handleSubmit = async () => {
-		loading = true
-		chatMessages = [...chatMessages, { role: 'user', content: query }]
-
-		const eventSource = new SSE(`/api/chat`, {
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			payload: JSON.stringify({ messages: [...chatMessages] })
-		})
-
-		query = ''
-
-		eventSource.addEventListener('error', handleErr)
-
-		eventSource.addEventListener('message', (e) => {
-			scrollToBottom()
-			try {
-				loading = false
-				if (e.data === '[DONE]') {
-					chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
-					answer = ''
-					return
-				}
-
-				const completionResponse = JSON.parse(e.data)
-				const [{ delta }] = completionResponse.choices
-
-				if (delta.content) {
-					answer = (answer ?? '') + delta.content
-				}
-			} catch (err) {
-				console.error(err)
-				handleErr(err)
-			}
-		})
-		eventSource.stream()
-		scrollToBottom()
-	}
-
-	function handleErr<T>(err: T) {
-		loading = false
-		query = ''
-		answer = ''
-		console.error(err)
-	}
 </script>
 
 <div class="flex flex-col pt-4 w-full px-8 items-center gap-2">
-	<h1 class="text-2xl font-bold w-full text-center">Chatty</h1>
+	<div>
+		<h1 class="text-2xl font-bold w-full text-center">Chatty</h1>
+		<p class="text-sm italic">Powered by gpt-3.5-turbo</p>
+	</div>
 	<div class="h-[500px] w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
 		<div class="flex flex-col gap-2">
 			<ChatMessage type="assistant" message="Hi, I'm Axel, how can I help?" />
-			{#each chatMessages as message}
-				<ChatMessage type={message.role} message={message.content} />
-			{/each}
-			{#if answer}
-				<ChatMessage type="assistant" message={answer} />
-			{/if}
-
-			{#if loading}
-				<ChatMessage type="assistant" message="Loading..." />
-			{/if}
+			<ChatMessage type="user" message="Hi" />
+			<ChatMessage type="assistant" message="Do you need something??" />
+			<ChatMessage type="user" message="Woah lose the attitude" />
 		</div>
-		<div class="" bind:this={scrollToDiv} id="scrollToDiv" />
+		<div class="" />
 	</div>
-	<form
-		class="flex w-full rounded-md gap-4 bg-gray-900 p-4"
-		on:submit|preventDefault={() => handleSubmit()}
-	>
-		<input type="text" class="input input-bordered w-full" bind:value={query} />
+	<form class="flex w-full rounded-md gap-4 bg-gray-900 p-4">
+		<input type="text" class="input input-bordered w-full" />
 		<button type="submit" class="btn btn-accent"> Send </button>
 	</form>
 </div>
