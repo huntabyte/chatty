11e920b30218ccceb98cd87788b2703729be32a6
Hunter Johnston <johnstonhuntera@gmail.com>
2023-03-04 14:37:01 -0500

    final

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,65 @@
 <script lang="ts">
 	import ChatMessage from '$lib/components/ChatMessage.svelte'
+	import type { ChatCompletionRequestMessage } from 'openai'
+	import { SSE } from 'sse.js'
+
+	let query: string = ''
+	let answer: string = ''
+	let loading: boolean = false
+	let chatMessages: ChatCompletionRequestMessage[] = []
+	let scrollToDiv: HTMLDivElement
+
+	function scrollToBottom() {
+		setTimeout(function () {
+			scrollToDiv.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
+		}, 100)
+	}
+
+	const handleSubmit = async () => {
+		loading = true
+		chatMessages = [...chatMessages, { role: 'user', content: query }]
+
+		const eventSource = new SSE('/api/chat', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			payload: JSON.stringify({ messages: chatMessages })
+		})
+
+		query = ''
+
+		eventSource.addEventListener('error', handleError)
+
+		eventSource.addEventListener('message', (e) => {
+			scrollToBottom()
+			try {
+				loading = false
+				if (e.data === '[DONE]') {
+					chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
+					answer = ''
+					return
+				}
+
+				const completionResponse = JSON.parse(e.data)
+				const [{ delta }] = completionResponse.choices
+
+				if (delta.content) {
+					answer = (answer ?? '') + delta.content
+				}
+			} catch (err) {
+				handleError(err)
+			}
+		})
+		eventSource.stream()
+		scrollToBottom()
+	}
+
+	function handleError<T>(err: T) {
+		loading = false
+		query = ''
+		answer = ''
+		console.error(err)
+	}
 </script>
 
 <div class="flex flex-col pt-4 w-full px-8 items-center gap-2">
@@ -9,15 +69,24 @@
 	</div>
 	<div class="h-[500px] w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
 		<div class="flex flex-col gap-2">
-			<ChatMessage type="assistant" message="Hi, I'm Axel, how can I help?" />
-			<ChatMessage type="user" message="Hi" />
-			<ChatMessage type="assistant" message="Do you need something??" />
-			<ChatMessage type="user" message="Woah lose the attitude" />
+			<ChatMessage type="assistant" message="Hello, ask me anything you want!" />
+			{#each chatMessages as message}
+				<ChatMessage type={message.role} message={message.content} />
+			{/each}
+			{#if answer}
+				<ChatMessage type="assistant" message={answer} />
+			{/if}
+			{#if loading}
+				<ChatMessage type="assistant" message="Loading.." />
+			{/if}
 		</div>
-		<div class="" />
+		<div class="" bind:this={scrollToDiv} />
 	</div>
-	<form class="flex w-full rounded-md gap-4 bg-gray-900 p-4">
-		<input type="text" class="input input-bordered w-full" />
+	<form
+		class="flex w-full rounded-md gap-4 bg-gray-900 p-4"
+		on:submit|preventDefault={() => handleSubmit()}
+	>
+		<input type="text" class="input input-bordered w-full" bind:value={query} />
 		<button type="submit" class="btn btn-accent"> Send </button>
 	</form>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,9 @@ const config = {
     })],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+            runtime: 'nodejs18.x'
+        })
 	}
 };
 
